2f8a81ecec1a60e0fce7d2bc2b286fd30765c019
Artem Vasilev <maxachkala69@gmail.com>
2024-03-08 20:33:26 +0200

    task 2 DONE

--- Lab 1. Nginx + PHP8-FPM/code/index.php
+++ Lab 1. Nginx + PHP8-FPM/code/index.php
@@ -11,3 +11,14 @@ $order = $order . " and Japanese Tree ";
   
  // Don't change the line below
  echo "\nYour order is: $very_bad_unclear_name.";
+ echo "<br>";
+ $tyoma = 3;
+echo $tyoma;
+$sveta = 7.5;
+echo "<br>";
+echo $sveta + $sveta - $tyoma;
+echo "<br>";
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+$difference = $lastMonth - $thisMonth;
+echo $difference;
